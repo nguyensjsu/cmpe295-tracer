ea9791c690cb3de0813cd4be3980a0ca1a2a4bd7
Devanshi Trivedi <42883110+trivedidevanshi@users.noreply.github.com>
2020-11-25 16:32:38 -0800

    Update LandingPage.jsx

--- codebase/distributed-tracer/dashboard/src/components/Home/LandingPage/LandingPage.jsx
+++ codebase/distributed-tracer/dashboard/src/components/Home/LandingPage/LandingPage.jsx
@@ -4,14 +4,14 @@ import {makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import BuildIcon from '@material-ui/icons/Build';
 import Typography from "@material-ui/core/Typography";
 
 import {HeroContainer} from "./HeroContainer";
 import {GuideCard} from "./GuideCard";
-import {Contributors} from "./Contributors";
-import Link from "@material-ui/core/Link";
+
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -23,13 +23,28 @@ const useStyles = makeStyles((theme) => ({
     guideCardsContainer: {
         margin: '70px 50px'
     },
-    iconColor:{
+    iconColor: {
         color: '#8FCAF9'
     },
     footer: {
-        padding:theme.spacing(6, 0),
+        flexGrow: 1,
+        padding: theme.spacing(5, 5),
         backgroundColor: '#000',
         color: '#bdbdbd'
+    },
+    paper: {
+        marginLeft: '20px',
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        background: 'none',
+        color: '#eee'
+    },
+    footerTitle: {
+        marginRight: '20px',
+        textAlign: 'center',
+        background: 'none',
+        color: '#eee',
+        fontSize: '20px'
     }
 }));
 
@@ -60,7 +75,8 @@ export const LandingPage = (props) => {
 
                 <div className={classes.guideCardsContainer} id='usage-id'>
                     <Grid container spacing={3}>
-                        <GuideCard avatar={installation.avatar} title={installation.title} linkData = {installation.linkData} link = {installation.link} >
+                        <GuideCard avatar={installation.avatar} title={installation.title}
+                                   linkData={installation.linkData} link={installation.link}>
                             <Typography variant="body1" gutterBottom>
                                 This is the installation guide<br/>
                                 <br/>
@@ -77,7 +93,8 @@ export const LandingPage = (props) => {
                             </Typography>
                         </GuideCard>
 
-                        <GuideCard avatar={usage.avatar} title={usage.title} linkData = {usage.linkData} link = {usage.link}>
+                        <GuideCard avatar={usage.avatar} title={usage.title} linkData={usage.linkData}
+                                   link={usage.link}>
                             <Typography variant="body1" gutterBottom>
                                 This is the usage guide<br/>
                                 <br/>
@@ -96,14 +113,35 @@ export const LandingPage = (props) => {
                     </Grid>
                 </div>
 
-                <Contributors/>
+                {/*<Contributors/>*/}
                 <footer className={classes.footer}>
-                        <Typography variant="h6" align="center" gutterBottom>
-                            CMPE 295B
-                        </Typography>
-                        <Typography variant="subtitle1" align="center" component="p">
-                            San Jose State University
-                        </Typography>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12} sm={6} >
+                            <Paper className={classes.footerTitle} style={{textAlign: 'right'}}>CMPE 295B</Paper>
+                            <Paper className={classes.footerTitle} style={{textAlign: 'right'}}>San Jose State University</Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Grid container spacing={1}>
+                                <Grid item xs={12} sm={6}>
+                                    <Paper className={classes.paper} style={{textAlign: 'right'}}>Contributors</Paper>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Paper className={classes.paper} style={{textAlign: 'left'}}>
+                                        Busi Pallavi Reddy<br/>
+                                        Prachi Chowksey<br/>
+                                        Maunil Swadas<br/>
+                                        Devanshi Trivedi
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Paper className={classes.paper} style={{textAlign: 'right'}}>Guide</Paper>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <Paper className={classes.paper} style={{textAlign: 'left'}}>Paul Nguyen</Paper>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </footer>
             </Container>
         </>
